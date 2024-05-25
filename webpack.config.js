9c65ff391b7e65cead0094285e7ecb4ac7593b0f
yanne <yanneduarte92@gmail.com>
2024-05-25 01:51:45 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  performance: {
+    ints: 'warning',
+    maxAssetSize: 2097152, // 2 MB in bytes
+    maxEntrypointSize: 2097152, // 2 MB in bytes
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle-report.html',
+    }),
+  ],
+};
